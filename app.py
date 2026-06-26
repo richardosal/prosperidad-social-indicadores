7d663d34f0b1d413a7170fdce56de7aec165d51b
richardosal <richardo3327@gmail.com>
2026-06-26 16:00:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,55 @@
+──────────────────────────────────────────────
+🏛️ Sistema Inteligente Prosperidad Social
+Predicción de Beneficiarios mediante IA
+──────────────────────────────────────────────
 
+👤 Información Personal
+
+📄 Tipo Documento
+[▼ Cédula]
+
+👨 Género
+[▼ Masculino]
+
+🎓 Nivel Escolaridad
+[▼ Bachiller]
+
+👵 Rango Edad
+[▼ 29-40]
+
+────────────────────────
+
+📍 Ubicación
+
+🌎 País
+
+🗺 Departamento
+
+🏙 Municipio
+
+────────────────────────
+
+💳 Información del Beneficio
+
+🏦 Bancarizado
+
+👨‍👩‍👧 Cantidad Beneficiarios
+
+💰 Rango Beneficio
+
+────────────────────────
+
+            🔵
+      PREDECIR BENEFICIO
+            🔵
+
+────────────────────────
+
+🟢 Resultado
+
+✔ Alta probabilidad
+
+Confianza:
+94.6%
+
+────────────────────────
